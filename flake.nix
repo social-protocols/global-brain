e47376ce7d55ec4b5c81294dc08e08f8d61b0687
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-01-17 19:30:31 +0100

    Add flake and direnv setup

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    # for `flake-utils.lib.eachSystem`
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ ];
+          config.allowUnfree = false;
+        };
+      in
+      {
+        devShells = {
+          default = with pkgs; pkgs.mkShellNoCC {
+            buildInputs = [
+              git
+              R
+              rPackages.renv
+            ];
+          };
+        };
+      }
+    );
+}
