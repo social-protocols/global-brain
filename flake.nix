6a8211692c3026374a0f8cf1a2b4ffde802bf49a
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-02-16 13:32:57 +0100

    Create action with nix installation

--- flake.nix
+++ flake.nix
@@ -1,8 +1,9 @@
 {
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-    # for `flake-utils.lib.eachSystem`
+    # for `flake-utils.lib.eachDefaultSystem`
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -12,26 +13,41 @@
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ ];
-          config.allowUnfree = false;
+          config = {
+            allowUnfree = false;
+            packageOverrides = super: let self = super.pkgs; in
+            {
+              rEnv = super.rWrapper.override {
+                packages = with self.rPackages; [
+                  rmarkdown
+                  yaml
+                  bookdown
+                  base64enc
+                  dplyr
+                  tidyr
+                  ggplot2
+                ];
+              };
+            };
+          };
         };
+        defaultBuildInputs = with pkgs; [
+          entr
+          just
+          git
+          pandoc
+          rEnv
+          texlive.combined.scheme-full
+        ];
       in
       {
-        devShells = {
-          default = with pkgs; pkgs.mkShellNoCC {
-            buildInputs = [
-              entr
-              just
-              git
-              R
-              rPackages.renv
-              rPackages.yaml
-              rPackages.bookdown
-              rPackages.base64enc
-              rPackages.dplyr
-              rPackages.tidyr
-              rPackages.ggplot2
-            ];
-          };
+        devShells.default = with pkgs; pkgs.mkShellNoCC {
+          name = "dev-shell";
+          buildInputs = defaultBuildInputs;
+        };
+        packages.default = with pkgs; pkgs.mkShellNoCC {
+          name = "pack";
+          buildInputs = defaultBuildInputs;
         };
       }
     );
